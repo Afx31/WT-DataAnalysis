6ba09a4d8c1c86193fae4bc499e85b24e95bc9a9
Afx31 <post4wjm@gmail.com>
2024-08-10 16:53:18 +1000

    WinForms DataVisulisation setup for Charts

--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,5 @@
+using System.Windows.Forms.DataVisualization.Charting;
+
 namespace WRD_DataAnalysis
 {
     public partial class Form1 : Form
@@ -5,6 +7,23 @@ namespace WRD_DataAnalysis
         public Form1()
         {
             InitializeComponent();
+            FormLogic();
+        }
+
+        public void FormLogic()
+        {
+            //var chartSeries = chart1.Series;
+
+            var series = new Series
+            {
+                Name = "Series1",
+                Color = System.Drawing.Color.Blue,
+                ChartType = SeriesChartType.Line
+            };
+            chart1.Series.Add(series);
+
+            for (int i = 0; i < 10; i++)
+                series.Points.AddXY(i, Math.Sin(i * 0.1));
         }
     }
 }
