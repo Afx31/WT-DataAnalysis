39a3eee98726e4dcdd6f4460b2ccb60915ec60ca
Afx31 <post4wjm@gmail.com>
2025-08-19 22:41:41 +1000

    ScatterPlots created dynamically now, although the contents is hardcoded for 4 of them, for now

--- WT-DataAnalysis-DatalogReview/Forms/ScatterPlotForm.cs
+++ WT-DataAnalysis-DatalogReview/Forms/ScatterPlotForm.cs
@@ -9,55 +9,128 @@ public partial class ScatterPlotForm : Form
 
     public ScatterPlotForm(CsvData csvData)
     {
+        this.WindowState = FormWindowState.Maximized;
+
         InitializeComponent();
 
         if (csvData != null)
         {
             _csvData = csvData;
-            ChartSetup();
-            MapDataToChart();
+            ScatterPlotSetup();
         }
     }
 
-    private void ChartSetup()
+    private void ScatterPlotSetup()
     {
-        ChartArea chartArea1 = new ChartArea();
-        chartArea1.Name = "ScatterPlotChartArea";
-        chartArea1.AxisX.MajorTickMark.Enabled = true;
-        chartArea1.AxisX.MinorTickMark.Enabled = true;
-        chartArea1.AxisX.MajorTickMark.Size = 0.5f;
-        chartArea1.AxisX.MinorTickMark.Size = 0.2f;
-        //chartArea1.Position = new ElementPosition(0, 0, 40, 40);
-        chartArea1.AxisX.LabelStyle.Format = "0.0";
-        chartArea1.AxisX.Minimum = 0;
+        TableLayoutPanel tableLayout = new()
+        {
+            Name = "ScatterPlotTableLayout",
+            Dock = DockStyle.Fill,
+            ColumnCount = 2,
+            RowCount = 2
+        };
+        // Make rows and columns evenly sized
+        for (int i = 0; i < 2; i++)
+        {
+            tableLayout.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50));
+            tableLayout.RowStyles.Add(new RowStyle(SizeType.Percent, 50));
+        }
+        this.Controls.Add(tableLayout);
 
-        // Additional:
-        chartArea1.BackColor = ColorTranslator.FromHtml("#1E1E1E");
-        chartArea1.AxisX.MajorGrid.LineColor = ColorTranslator.FromHtml("#333333"); // Originally #333333
-        chartArea1.AxisX.MinorGrid.LineColor = ColorTranslator.FromHtml("#4D4D4D");
-        chartArea1.AxisX.MajorTickMark.LineColor = ColorTranslator.FromHtml("#FFFFFF");
-        chartArea1.AxisX.MinorTickMark.LineColor = ColorTranslator.FromHtml("#FFFFFF");
-        chartArea1.AxisX.LabelStyle.ForeColor = ColorTranslator.FromHtml("#CCCCCC");
-        chartArea1.AxisX.TitleForeColor = ColorTranslator.FromHtml("#FFFFFF");
-        chartArea1.AxisY.MajorGrid.LineColor = ColorTranslator.FromHtml("#333333");
-        chartArea1.AxisY.MinorGrid.LineColor = ColorTranslator.FromHtml("#4D4D4D");
-        chartArea1.AxisY.MajorTickMark.LineColor = ColorTranslator.FromHtml("#FFFFFF");
-        chartArea1.AxisY.MinorTickMark.LineColor = ColorTranslator.FromHtml("#FFFFFF");
-        chartArea1.AxisY.LabelStyle.ForeColor = ColorTranslator.FromHtml("#CCCCCC");
-        chartArea1.AxisY.TitleForeColor = ColorTranslator.FromHtml("#FFFFFF");
-        
-        chartArea1.AxisY.Title = "Oil Pressure";
-        chartArea1.AxisY.TextOrientation = TextOrientation.Stacked;
-        chartArea1.AxisY.TitleFont = new Font("Arial", 18);
 
-        // Zoomable
-        chartArea1.AxisX.ScaleView.Zoomable = true;
-        chartArea1.AxisY.ScaleView.Zoomable = true;
-        
-        chart1.ChartAreas.Add(chartArea1);
+        string[] zAxisValues1 = { "20", "40", "60", "70", "80", "90", "110", "120", "140" };
+        ChartSetup([1, 0, 0], "Oil Pressure Behavior Across RPM with Temperature Influence",
+                    ["RPM", "Oil Pressure", "Oil Temp"], _csvData.ListRpm, _csvData.ListAnalog1, _csvData.ListAnalog0, zAxisValues1);
+
+        string[] zAxisValues2 = { "1000", "2000", "3000", "4000", "5000", "6000", "7000", "8000", "9000" };
+        ChartSetup([2, 1, 0], "Oil Temperature Rise Over Time with Engine Load Context",
+                    ["Hertz", "Oil Temp", "RPM"], _csvData.ListHertzTime, _csvData.ListAnalog0, _csvData.ListRpm, zAxisValues2);
+
+        string[] zAxisValues3 = { "20", "40", "60", "70", "80", "90", "110", "120", "140" };
+        ChartSetup([3, 0, 1], "Engine Cooling Response Across RPM",
+                    ["RPM", "ECT", "Oil Temp"], _csvData.ListRpm, _csvData.ListECT, _csvData.ListAnalog0, zAxisValues3);
+
+        string[] zAxisValues4 = { "10", "20", "30", "40", "50", "60", "70", "80", "90" };
+        ChartSetup([4, 1, 1], "Throttle Application vs RPM with Oil Pressure Monitoring",
+                    ["RPM", "TPS", "Oil Pressure"], _csvData.ListRpm, _csvData.ListTPS, _csvData.ListAnalog1, zAxisValues4);
     }
 
-    private void BuildScatterPlotChart(string chartName, List<double> xAxis, List<double> yAxis, List<double> zAxis, string[] values)
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="tlpGrid">[0] == Chart Number || [1] == Column Number || [2] == Row Number</param>
+    /// <param name="chartName"></param>
+    /// <param name="axisTitles">[0] == xAxis || [1] == yAxis || [2] == zAxis</param>
+    /// <param name="xAxis"></param>
+    /// <param name="yAxis"></param>
+    /// <param name="zAxis"></param>
+    /// <param name="zAxisValues"></param>
+    private void ChartSetup(int[] tlpGrid, string chartName, string[] axisTitles, List<double> xAxis, List<double> yAxis, List<double> zAxis, string[] zAxisValues)
+    {
+        #region Chart
+        Chart chart = new()
+        {
+            Name = "chart_" + tlpGrid[0],
+            BackColor = Color.FromArgb(0, 10, 15),
+            BorderlineColor = Color.FromArgb(45, 45, 45),
+            Location = new Point(0, 0),
+            Margin = new Padding(0),
+            Size = new Size(1075, 450),
+            TabIndex = 0,
+            Dock = DockStyle.Fill
+        };
+        chart.Titles.Add(chartName);
+        chart.Titles[0].Font = new Font("Arial", 18);
+        chart.Titles[0].ForeColor = ColorTranslator.FromHtml("#FFFFFF");
+        chart.MouseMove += Chart_MouseMoveOrClick;
+        #endregion
+
+        #region Chart Area
+        ChartArea chartArea = new();
+        chartArea.Name = "ScatterPlotChartArea";
+        chartArea.AxisX.MajorTickMark.Enabled = true;
+        chartArea.AxisX.MinorTickMark.Enabled = true;
+        chartArea.AxisX.MajorTickMark.Size = 0.5f;
+        chartArea.AxisX.MinorTickMark.Size = 0.2f;
+        //chartArea.Position = new ElementPosition(0, 0, 40, 40);
+        chartArea.AxisX.LabelStyle.Format = "0.0";
+        chartArea.AxisX.Minimum = 0;
+
+        // Additional:
+        chartArea.BackColor = ColorTranslator.FromHtml("#1E1E1E");
+        chartArea.AxisX.MajorGrid.LineColor = ColorTranslator.FromHtml("#333333"); // Originally #333333
+        chartArea.AxisX.MinorGrid.LineColor = ColorTranslator.FromHtml("#4D4D4D");
+        chartArea.AxisX.MajorTickMark.LineColor = ColorTranslator.FromHtml("#FFFFFF");
+        chartArea.AxisX.MinorTickMark.LineColor = ColorTranslator.FromHtml("#FFFFFF");
+        chartArea.AxisX.LabelStyle.ForeColor = ColorTranslator.FromHtml("#CCCCCC");
+        chartArea.AxisX.Title = axisTitles[0];
+        chartArea.AxisX.TitleFont = new Font("Arial", 12);
+        chartArea.AxisX.TitleForeColor = ColorTranslator.FromHtml("#FFFFFF");
+
+        chartArea.AxisY.MajorGrid.LineColor = ColorTranslator.FromHtml("#333333");
+        chartArea.AxisY.MinorGrid.LineColor = ColorTranslator.FromHtml("#4D4D4D");
+        chartArea.AxisY.MajorTickMark.LineColor = ColorTranslator.FromHtml("#FFFFFF");
+        chartArea.AxisY.MinorTickMark.LineColor = ColorTranslator.FromHtml("#FFFFFF");
+        chartArea.AxisY.LabelStyle.ForeColor = ColorTranslator.FromHtml("#CCCCCC");
+        chartArea.AxisY.Title = axisTitles[1];
+        chartArea.AxisY.TitleFont = new Font("Arial", 12);
+        chartArea.AxisY.TitleForeColor = ColorTranslator.FromHtml("#FFFFFF");
+        chartArea.AxisY.TextOrientation = TextOrientation.Stacked;
+
+        // Zoomable
+        chartArea.AxisX.ScaleView.Zoomable = true;
+        chartArea.AxisY.ScaleView.Zoomable = true;
+        
+        chart.ChartAreas.Add(chartArea);
+        #endregion
+
+        BuildScatterPlotDataStructure(chart, xAxis, yAxis, zAxis, axisTitles[2], zAxisValues);
+
+        TableLayoutPanel tlp = this.Controls["ScatterPlotTableLayout"] as TableLayoutPanel;
+        tlp.Controls.Add(chart, tlpGrid[1], tlpGrid[2]);
+    }
+
+    private void BuildScatterPlotDataStructure(Chart chart, List<double> xAxis, List<double> yAxis, List<double> zAxis, string zAxisTitle, string[] zAxisValues)
     {
         #region Legends
         Legend mainLegend = new()
@@ -65,10 +138,10 @@ public partial class ScatterPlotForm : Form
             Name = "mainLegend",
             LegendStyle = LegendStyle.Column,
             ForeColor = Color.White,
-            BackColor = Color.Transparent
+            BackColor = Color.Transparent,
+            Font = new Font("Arial", 10)
         };
-        chart1.Legends.Add(mainLegend);
-        chart1.Legends["mainLegend"].Font = new Font("Arial", 10);
+        chart.Legends.Add(mainLegend);
 
         static Color GetMyScatterPlotColour(int i)
         {
@@ -101,17 +174,17 @@ public partial class ScatterPlotForm : Form
         {
             LegendItem li = new()
             {
-                Name = "<= " + values[i],
+                Name = "<= " + zAxisValues[i],
                 Color = GetMyScatterPlotColour(i)
             };
-            chart1.Legends["mainLegend"].CustomItems.Add(li);
+            chart.Legends["mainLegend"].CustomItems.Add(li);
         }
         #endregion
 
         #region DataPoints
         Series series = new()
         {
-            Name = chartName,
+            Name = zAxisTitle,
             IsValueShownAsLabel = false,
             ChartArea = "ScatterPlotChartArea",
             //Color = ColorTranslator.FromHtml("#FF5722"),
@@ -122,15 +195,15 @@ public partial class ScatterPlotForm : Form
         };
 
         #region Convert these first so we're not doing it constantly throughout the loop
-        double val1 = Convert.ToDouble(values[0]);
-        double val2 = Convert.ToDouble(values[1]);
-        double val3 = Convert.ToDouble(values[2]);
-        double val4 = Convert.ToDouble(values[3]);
-        double val5 = Convert.ToDouble(values[4]);
-        double val6 = Convert.ToDouble(values[5]);
-        double val7 = Convert.ToDouble(values[6]);
-        double val8 = Convert.ToDouble(values[7]);
-        double val9 = Convert.ToDouble(values[8]);
+        double val1 = Convert.ToDouble(zAxisValues[0]);
+        double val2 = Convert.ToDouble(zAxisValues[1]);
+        double val3 = Convert.ToDouble(zAxisValues[2]);
+        double val4 = Convert.ToDouble(zAxisValues[3]);
+        double val5 = Convert.ToDouble(zAxisValues[4]);
+        double val6 = Convert.ToDouble(zAxisValues[5]);
+        double val7 = Convert.ToDouble(zAxisValues[6]);
+        double val8 = Convert.ToDouble(zAxisValues[7]);
+        double val9 = Convert.ToDouble(zAxisValues[8]);
         #endregion
 
         for (int i = 0; i < xAxis.Count; i++)
@@ -160,26 +233,19 @@ public partial class ScatterPlotForm : Form
             series.Points[i].Color = colourToUse;
         }
 
-        chart1.ChartAreas["ScatterPlotChartArea"].AxisY.Interval = 10;
-        chart1.Series.Add(series);
+        chart.ChartAreas["ScatterPlotChartArea"].AxisY.Interval = 10;
+        chart.Series.Add(series);
         #endregion
     }
 
-    private void MapDataToChart()
+    private void Chart_MouseMoveOrClick(object sender, MouseEventArgs e)
     {
-        // RPM | Oil Pressure | Oil Temp
-        string[] str1 = { "20", "40", "60", "70", "80", "90", "110", "120", "140" };
-        BuildScatterPlotChart("Oil reee", _csvData.ListRpm, _csvData.ListAnalog1, _csvData.ListAnalog0, str1);
+        Chart chart = sender as Chart;
+        if (chart == null) return;
 
-        string[] str2 = { "1000", "2000", "3000", "4000", "5000", "6000", "7000", "8000", "9000" };
-        //BuildScatterPlotChart("RPM", _csvData.ListHertzTime, _csvData.ListAnalog0, _csvData.ListRpm, str2);
-    }
-
-    private void Chart1_MouseMoveOrClick(object sender, MouseEventArgs e)
-    {
-        if (chart1 != null && e.X > 0)
+        if (chart != null && e.X > 0)
         {
-            ChartArea ca = chart1.ChartAreas[0];
+            ChartArea ca = chart.ChartAreas[0];
 
             if (ca.AxisX.StripLines.Count > 0)
                 ca.AxisX.StripLines.RemoveAt(0);
